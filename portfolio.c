4ccb15f3f30c761544ec80052bebea4a6e528809
genminias <gaminias@crimson.ua.edu>
2019-03-02 16:55:59 -0600

    added .c and .h files for classes

--- portfolio.c
+++ portfolio.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 //#include <ctype.h>
-//#include "classes.h" //MAKE THIS FILE
-//#include "classes.c" //MAKE THIS FILE
+#include "classes.h"
 
 //MAKE A MAKEFILE :)
 
@@ -13,12 +12,11 @@ int main(void) {
     printf("Welcome to your movie catalog!\n");
     printf("What would you like to name your first catalog? (must be less than 16 characters): ");
     scanf("%s", catalogName);
-    while (strlen(catalogName) > 15) {
+    if (strlen(catalogName) > 15) {
         printf("Error: catalog name must be less than 16 characters, please try again\n");
         return -1;
     }
     strcat(catalogName, ".txt");
-    printf("%s\n", catalogName);
 
     //FILE *logFile = fopen(catalogName, "w");
 
