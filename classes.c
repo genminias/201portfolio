c020017b8dc1ad7b31c16aef5f0120a95e99df0f
genminias <gaminias@crimson.ua.edu>
2019-03-27 00:52:59 -0500

    IT NEVER ENDS

--- classes.c
+++ classes.c
@@ -496,7 +496,7 @@ int catalogMenuOptions(char *menuItem, char *user) {
             if (fgets(val, 50, stdin) == NULL) {
                 exit(1);
             }
-            tries++
+            tries++;
         } while (strchr(val, '\n') == NULL);
         if (tries != 1) {
             strcpy(val, "lolllll");
